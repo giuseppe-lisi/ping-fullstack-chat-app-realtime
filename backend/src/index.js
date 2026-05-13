bc718ee4e15ecfd58c03eed31cfd9d64fbe027d7
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-13 13:28:59 +0200

    added entrypoint to api

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser.js"
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use(cookieParser);
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port: `, PORT);
+    connectDB();
+});
