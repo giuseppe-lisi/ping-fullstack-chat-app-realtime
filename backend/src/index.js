6ba618f32d382ecd5cd4600febaba0676f8c12fe
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-13 14:20:03 +0200

    added cookie parser middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser.js"
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
