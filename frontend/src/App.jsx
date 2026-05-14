39dc1fd384268eb009213ca1474c6f0447cf6676
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-14 15:40:56 +0200

    added base routes and auth check on page load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 function App() {
+    const { authUser, checkAuth } = useAuthStore();
 
-  return (
-    <>
-      <div>Landing</div>
-    </>
-  )
+    useEffect(() => {
+        checkAuth();
+    }, []);
+
+    console.log(authUser);
+
+    return (
+        <div>
+            <Navbar></Navbar>
+
+            <Routes>
+                <Route path="/" Component={Homepage} />
+                <Route path="/signup" Component={SignUpPage} />
+                <Route path="/login" Component={LoginPage} />
+                <Route path="/settings" Component={SettingsPage} />
+                <Route path="/profile" Component={ProfilePage} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
