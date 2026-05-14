acf35d64800721d645d87c2bee2a72bc148c6f05
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-14 21:43:22 +0200

    added states to zustand global state to handle user auth requests to the backend aswell as profile picture update

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,23 +1,80 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
-    isSigningUp: false,
     isLoggingIn: false,
+    isSigningUp: false,
     isUpdatingProfile: false,
 
     isCheckingAuth: true,
 
     checkAuth: async () => {
         try {
-            const res = axiosInstance.get("/auth/check");
+            const res = await axiosInstance.get("/auth/check");
 
             set({ authUser: res.data });
         } catch (error) {
             set({ authUser: null });
         } finally {
             set({ isCheckingAuth: false });
+        }
+    },
+
+    signup: async (formData) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", formData);
+            toast.success("Account created!");
+
+            set({ authUser: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+
+    login: async (formData) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", formData);
+            toast.success("Logged in!");
+
+            set({ authUser: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Successfully logged out!");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            console.log("before put request");
+            
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            console.log("after");
+            set({ authUser: res.data });
+            toast.success("Profile picture updated successfully");
+        } catch (error) {
+            console.log("Something went wrong: ", error);
+            const errorMessage = error.response?.data?.message;
+            toast.error(errorMessage || "Server error");
+        } finally {
+            set({ isUpdatingProfile: false });
         }
     },
 }));
