c3fe0be62e131a5fe4e00274813af4ce65cdd1f3
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-14 13:16:10 +0200

    route to check if user is still logged in after page refreshes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -11,5 +11,8 @@ router.post("/logout", logout);
 
 // check if user is authenticated, then let them update the profile
 router.put("/update-profile", protectRoute, updateProfile);
+
+// check user auth - generic, for page refreshes
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
