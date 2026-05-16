43a24f8c704cb33a81f2e4fcb81f317dc72feb19
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-16 17:35:57 +0200

    the listener is not the server with socket io attached to it

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,32 +5,32 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
-
+import { app, server } from "./lib/socket.js";
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT;
 
 // increase file upload limit
-app.use(express.json({ limit: "2mb" })); 
+app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ limit: "2mb", extended: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5174",
+        origin: ["http://localhost:5173", "http://localhost:5174"],
         credentials: true,
     }),
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, (error) => {
+server.listen(PORT, (error) => {
     console.log(`App running on port: `, PORT);
     connectDB();
     if (error) console.log(error);
