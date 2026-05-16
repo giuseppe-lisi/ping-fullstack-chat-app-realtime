0e66d9ad566751cff24b75af6f9d41fa6b2da97b
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-16 18:18:51 +0200

    frontend in production is now served directly by the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
 import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve;
 
 // increase file upload limit
 app.use(express.json({ limit: "2mb" }));
@@ -26,9 +28,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+// in production, we serve api and frontend in the same place
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, (error) => {
     console.log(`App running on port: `, PORT);
